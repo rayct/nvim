9d30e5b2cee6e57af4a9bc01d63358494a5b27a5
Raymond Turner <ray@Raymonds-MacBook-Pro.local>
2022-11-22 17:23:21 +0000

    Completed File

--- lua/rayct/plugins/autopairs.lua
+++ lua/rayct/plugins/autopairs.lua
@@ -2,9 +2,13 @@
 local autopairs_setup, autopairs = pcall(require, "nvim-autopairs")
 if not autopairs_setup then
   return
-enk_ts = true, -- enable treesitter
+end
+
+-- configure autopairs
+autopairs.setup({
+  check_ts = true, -- enable treesitter
   ts_config = {
-   lua = { "string" }, -- don't add pairs in lua string treesitter nodes
+    lua = { "string" }, -- don't add pairs in lua string treesitter nodes
     javascript = { "template_string" }, -- don't add pairs in javscript template_string treesitter nodes
     java = false, -- don't check treesitter on java
   },
